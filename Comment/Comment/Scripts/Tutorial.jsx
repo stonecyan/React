25455de908710349dce797f8421f55c8217bda4d
Stone Yan <stoneyan1995@gmail.com>
2018-09-27 11:05:21 -0700

    React MVC Comment tutorial

---
+++ Comment/Comment/Scripts/Tutorial.jsx
@@ -0,0 +1,106 @@
+﻿const data = [
+    { Id: 1, Author: "Daniel Lo Nigro", Text: "Hello ReactJS.NET World!" },
+    { Id: 2, Author: "Pete Hunt", Text: "This is one comment" },
+    { Id: 3, Author: "Jordan Walke", Text: "This is *another* comment" }
+];
+
+class CommentBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { data: [] };
+    }
+    loadCommentsFromServer() {
+        const xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = () => {
+            const data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        };
+        xhr.send();
+    }
+    componentDidMount() {
+        this.loadCommentsFromServer();
+        window.setInterval(() => this.loadCommentsFromServer(), this.props.pollInterval);
+    }
+    render() {
+    return (
+        <div className="commentBox">
+            <h1>Comments</h1>
+            <CommentList data={this.state.data} />
+            <CommentForm />
+      </div>
+    );
+  }
+}
+
+class CommentList extends React.Component {
+    render() {
+        const commentNodes = this.props.data.map(comment => (
+            <Comment author={comment.Author} key={comment.Id} >
+                {comment.Text}
+            </Comment>
+        ));
+        return (
+            <div className="commentList">
+                {commentNodes}
+            </div>
+        );
+    }
+}
+
+class CommentForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { author: '', text: '' };
+        this.handleAuthorChange = this.handleAuthorChange.bind(this);
+        this.handleTextChange = this.handle.TextChange.bind(this);
+    }
+    handleAuthorChange(e) {
+        this.setState({ author: e.target.value });
+    }
+    handleTextChange(e) {
+        this.setState({ text: e.target.value });
+    }
+    render() {
+        return (
+            <form className="commentForm">
+                <input
+                    type="text"
+                    placeholder="Your name"
+                    value={this.state.author}
+                    onChange={this.handleAuthorChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Say something..."
+                    value={this.state.text}
+                    onChange={this.handleTextChange}
+                />
+                <input type="submit" value="Post" />
+            </form>
+        );
+    }
+}
+
+class Comment extends React.Component {
+    rawMarkup() {
+        const md = new Remarkable();
+        const rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    }
+    render() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <CommentBox url="/comments" pollInterval={2000}/>,
+  document.getElementById('content')
+);
